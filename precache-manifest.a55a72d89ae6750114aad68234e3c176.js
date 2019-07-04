aaf6e46c1ffaeab2390eaa02d04337a7e565f1a8
Facundo Benitez <facundob91@gmail.com>
2019-07-04 20:33:07 -0300

    Updates

---
+++ precache-manifest.a55a72d89ae6750114aad68234e3c176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d619d6043c300a6d3c1593609379096",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "b4181d9e8a37e430e9f0",
+    "url": "/weather-react-app/static/css/main.a0e23869.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/weather-react-app/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "b4181d9e8a37e430e9f0",
+    "url": "/weather-react-app/static/js/main.6ca18d41.chunk.js"
+  },
+  {
+    "revision": "7f73905e939fe825089b",
+    "url": "/weather-react-app/static/js/runtime~main.608e19b5.js"
+  },
+  {
+    "revision": "4411db90e7cc3b397ba3f72b4f8460ee",
+    "url": "/weather-react-app/static/media/bg.4411db90.jpg"
+  }
+]);
